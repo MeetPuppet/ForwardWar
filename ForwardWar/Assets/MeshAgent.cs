0652e703fac62a1654d9b31e101fa360c842940a
ChoiChiSong <zw159@naver.com>
2021-03-31 17:39:24 +0900

    Add - ruin model        - Nav Agent & bake asset        - NavAgent.cs

---
+++ ForwardWar/Assets/MeshAgent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeshAgent : MonoBehaviour
+{
+    public Transform goal;
+    private NavMeshAgent agent = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MovePosition(Transform MoveDest)
+    {
+        goal = MoveDest;
+        agent.destination = goal.position;
+    }
+}
